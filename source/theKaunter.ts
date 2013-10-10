9d7c93a4260bcd4b7798865f6f15b20719815e03
jtornero <jtorlistas@gmail.com>
2013-10-10 23:45:01 +0200

    Added copyright/license to files. Added source file for translation

---
+++ source/theKaunter.ts
@@ -0,0 +1,93 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!--
+TheKaunter. A versatile differential counter.
+
+Copyright 2013 Jorge Tornero Núñez
+
+This file is part of TheKaunter
+
+TheKaunter is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+TheKaunter is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with TheKaunter.  If not, see <http://www.gnu.org/licenses/>.
+-->
+<!DOCTYPE TS><TS version="2.0" language="es_ES" sourcelanguage="en_GB">
+<context>
+    <name>cellCounter</name>
+    <message>
+        <location filename="theKaunter.py" line="24"/>
+        <source>TheKaunter: A versatile differential counter</source>
+        <translation>TheKaunter: Un contador diferencial versátil </translation>
+    </message>
+    <message>
+        <location filename="theKaunter.py" line="25"/>
+        <source>Export to &amp;File</source>
+        <translation>Exportar a &amp;Archivo</translation>
+    </message>
+    <message>
+        <location filename="theKaunter.py" line="26"/>
+        <source>Export to &amp;Clipboard</source>
+        <translation>Exportar al &amp;Portapapeles</translation>
+    </message>
+    <message>
+        <location filename="theKaunter.py" line="35"/>
+        <source>&lt;center&gt;&lt;b&gt;%s&lt;/b&gt;&lt;/center&gt;</source>
+        <translation>&lt;center&gt;&lt;b&gt;%s&lt;/b&gt;&lt;/center&gt;</translation>
+    </message>
+    <message>
+        <location filename="theKaunter.py" line="48"/>
+        <source>Total %s</source>
+        <translation>Total %s</translation>
+    </message>
+    <message>
+        <location filename="theKaunter.py" line="78"/>
+        <source>Confirmation Dialog</source>
+        <translation>Diálogo de Confirmación</translation>
+    </message>
+    <message>
+        <location filename="theKaunter.py" line="79"/>
+        <source>&lt;p align=&apos;center&apos;&gt;&lt;b&gt;The Kaunter is
+                                   going to be resetted.&lt;/b&gt;&lt;br&gt;&lt;br&gt;
+                                   All current information is going to be 
+                                   lost.&lt;br&gt;&lt;br&gt;&lt;b&gt;Are you sure?&lt;/b&gt;&lt;/p&gt;</source>
+        <translation>&lt;p align=&apos;center&apos;&gt;&lt;b&gt;Se va a reiniciar TheKaunter.&lt;/b&gt;&lt;br&gt;&lt;br&gt;
+Los datos actuales se perderán&lt;br&gt;&lt;br&gt;&lt;b&gt;¿Está seguro?&lt;/b&gt;&lt;/p&gt;</translation>
+    </message>
+    <message>
+        <location filename="theKaunter.py" line="83"/>
+        <source>Proceed</source>
+        <translation>Proceder</translation>
+    </message>
+    <message>
+        <location filename="theKaunter.py" line="84"/>
+        <source>Cancel</source>
+        <translation>Cancelar</translation>
+    </message>
+    <message>
+        <location filename="theKaunter.py" line="116"/>
+        <source>Select file for exporting data</source>
+        <translation>Seleccione un fichero para exportar los datos</translation>
+    </message>
+</context>
+<context>
+    <name>countingWidget</name>
+    <message>
+        <location filename="theKaunter.py" line="169"/>
+        <source>&lt;center&gt;%s&lt;br&gt;%s&lt;/center&gt;</source>
+        <translation>&lt;center&gt;%s&lt;br&gt;%s&lt;/center&gt;</translation>
+    </message>
+    <message>
+        <location filename="theKaunter.py" line="172"/>
+        <source>&lt;center&gt;&lt;b&gt;%s&lt;br&gt;%s&lt;/b&gt;&lt;/center&gt;</source>
+        <translation>&lt;center&gt;&lt;b&gt;%s&lt;br&gt;%s&lt;/b&gt;&lt;/center&gt;</translation>
+    </message>
+</context>
+</TS>
